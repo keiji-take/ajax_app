f3c69856f825ac6f0714e3584a65d2ce46c5e729
keiji-take <social.200512@gmial.com>
2020-10-31 17:26:35 +0900

    ajax_appの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
+  root to: 'posts#index'
+  # get 'posts/new', to: 'posts#new'
   #ここでのgetとpostの意味がわからない
+  # →これはHTTPメソッドを使って何をやるのかを定義している
   #なぜここでこれらを定義するのか？
+  # →HTTPメソッドそれぞれに役割があり、getだとデータの取得。postだとデータの送信(主に新規登録)を意味する
   post 'posts', to: 'posts#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
