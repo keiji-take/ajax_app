2b544d75a04c5e4c8080120b75091b4bf7f6ca85
keiji-take <social.200512@gmial.com>
2020-11-01 02:35:21 +0900

    既読機能サーバーサイド準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,19 @@ class PostsController < ApplicationController
     Post.create(content:params[:content])
     redirect_to action: :index
   end
+  def checked
+    # 既読したメモのidが欲しいので該当するレコードを取得
+    post = Post.find(params[:id])
+    if post.checked
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+    # 変更したレコードを取得
+    item = Post.find(params[:id])
+    # JSON形式でchecked.jsに返却
+    render json: {post: item}
+  end
 
 end
 
